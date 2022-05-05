265b5a44ed41bee8b3e3d955a3745db239a2fa88
Hisam Fahri <me@hisamafahri.com>
2022-05-05 13:43:13 +0700

    feat(lcs): add main command handler

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "lcs",
+	Short: "lcs is a very fast license generator",
+	Long:  `A fast and easy to use license generator, for you!`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("This is LCS")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/hisamafahri/lcs/cmd"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	cmd.Execute()
 }
